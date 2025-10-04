f34da77f0819c1d62826234a1f920dd522401321
bollokakos-galactic <akos.bollok@galacticsyn.com>
2025-10-04 07:16:23 +0200

    added dependencies, virtual env and basic gameloop

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
